1f7dcd31df81bffb13389ead70ae26500cdaf71f
Ricardo Duque <ricardoduque.programador@gmail.com>
2025-10-20 12:11:16 -0400

    removed whatsapp

--- src/components/home/PopoverBanner.tsx
+++ src/components/home/PopoverBanner.tsx
@@ -17,7 +17,7 @@ export default function PopoverBanner({
   const slide = banner[0];
 
   useEffect(() => {
-    if (isOncePopup) {
+    if (isOncePopup && slide?.images) {
       const savedImage = localStorage.getItem('oncePopup');
       if (savedImage) {
         if (savedImage === slide.images) return;
@@ -27,44 +27,42 @@ export default function PopoverBanner({
     } else {
       setShowBanner(true);
     }
-  }, [isOncePopup, slide.images]);
+  }, [isOncePopup, slide]);
 
-  console.log('slide', slide);
-  console.log('showBanner', showBanner);
-
-  return (
-    showBanner && (
-      <div className='fixed top-0 left-0 z-[99999999] bg-black/50 w-full h-full flex items-center justify-center flex-col '>
-        <div className='container flex flex-col'>
-          <div className='w-full container flex justify-end mb-4'>
-            <button
-              className=' text-white text-3xl font-bold z-10'
-              onClick={() => setShowBanner(false)}
-            >
-              &times;
-            </button>
-          </div>
-          <div>
-            <picture className='relative w-full h-[420px]'>
-              <source
-                srcSet={`${apiUrl}/file/${slide.mobileImage || slide.images}`}
-                media='(max-width: 768px)'
-              />
-              <img
-                src={`${apiUrl}/file/${slide.images}`}
-                alt={slide?.description || 'mercado'}
-                width={2048}
-                height={1024}
-                style={{
-                  width: '100%',
-                  height: '420px',
-                  objectFit: 'cover',
-                }}
-              />
-            </picture>
+  if (slide)
+    return (
+      showBanner && (
+        <div className='fixed top-0 left-0 z-[99999999] bg-black/50 w-full h-full flex items-center justify-center flex-col '>
+          <div className='container flex flex-col'>
+            <div className='w-full container flex justify-end mb-4'>
+              <button
+                className=' text-white text-3xl font-bold z-10'
+                onClick={() => setShowBanner(false)}
+              >
+                &times;
+              </button>
+            </div>
+            <div>
+              <picture className='relative w-full h-[420px]'>
+                <source
+                  srcSet={`${apiUrl}/file/${slide.mobileImage || slide.images}`}
+                  media='(max-width: 768px)'
+                />
+                <img
+                  src={`${apiUrl}/file/${slide.images}`}
+                  alt={slide?.description || 'mercado'}
+                  width={2048}
+                  height={1024}
+                  style={{
+                    width: '100%',
+                    height: '420px',
+                    objectFit: 'cover',
+                  }}
+                />
+              </picture>
+            </div>
           </div>
         </div>
-      </div>
-    )
-  );
+      )
+    );
 }
