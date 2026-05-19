745858d93bbc215bf6eae34082d0730f11dc6630
Ricardo Duque <ricardoduque.programador@gmail.com>
2026-05-18 21:25:00 -0400

    feat/adding-binance-and-image-to-methods

--- src/components/cart/SelectPaymentMethod.tsx
+++ src/components/cart/SelectPaymentMethod.tsx
@@ -33,6 +33,23 @@ const paymentTypeOptions = [
     label: 'Pago Movil',
     Icon: DevicePhoneMobileIcon,
   },
+  {
+    type: ETypePaymentMethods.Binance,
+    label: 'Binance',
+    Icon: () => (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+      >
+        <path
+          fill='currentColor'
+          d='m7.068 12l-2.03 2.03L3.003 12l2.03-2.03zm4.935-4.935l3.482 3.483l2.03-2.03L12.003 3L6.485 8.518l2.03 2.03zm6.964 2.905L16.937 12l2.03 2.03l2.03-2.03zm-6.964 6.965L8.52 13.452l-2.03 2.03L12.003 21l5.512-5.518l-2.03-2.03zm0-2.905l2.03-2.03l-2.03-2.03L9.967 12z'
+        />
+      </svg>
+    ),
+  },
 ];
 
 export default function SelectPaymentMethod() {
